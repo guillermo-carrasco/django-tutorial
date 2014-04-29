4e8c17887550e2edb24f7e9ae41cb2890c23fd41
Guillermo Carrasco <guille.ch.88@gmail.com>
2014-04-29 11:11:57 +0200

    Add some docs and gitignore

--- polls/admin.py
+++ polls/admin.py
@@ -7,8 +7,13 @@ class ChoiceInline(admin.TabularInline):
     extra = 3
 
 class PollAdmin(admin.ModelAdmin):
+    # Show specific fields of the object in the Admin site
     list_display = ('question', 'pub_date', 'was_published_recently')
+    # Add side bar filter for date
     list_filter = ['pub_date']
+    # And a search field, by question
+    search_fields = ['question']
+    # This will separate fields in different sets in the Admin page
     fieldsets = [
         (None, {'fields': ['question']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
