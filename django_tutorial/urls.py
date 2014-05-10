052a0a80471b2fa52ba46beedf5a66375b84e28e
Guillermo Carrasco <guille.ch.88@gmail.com>
2014-05-10 11:53:00 +0200

    Finished part 3 of tutorial. Views and URLs

--- django_tutorial/urls.py
+++ django_tutorial/urls.py
@@ -4,9 +4,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'django_tutorial.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    # On include() Django will forward the url to the URLConf object chopping
+    # the part that matched initially. So for example in this case won't send 
+    # /polls/<whatever>, but just <whatever> to polls.urls
+    url(r'^polls/', include('polls.urls', namespace='polls')),
     url(r'^admin/', include(admin.site.urls)),
 )
